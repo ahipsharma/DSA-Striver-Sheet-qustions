c7f35b4b84d73497f5ab8930ea6e04b33bf88956
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2022-12-25 13:23:51 +0530

    DSA-Sheet Questions

---
+++ Trees/Medium Problems/Height of a BST.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int l = maxDepth(root->left);
+        int r = maxDepth(root->right);
+        return 1+max(l, r);
+    }
+};
