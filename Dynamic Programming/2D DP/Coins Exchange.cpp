14f0528565897933d533d59e0bf244230c096e11
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-01-23 11:58:27 +0530

    DSA-Sheet Question.

---
+++ Dynamic Programming/2D DP/Coins Exchange.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
