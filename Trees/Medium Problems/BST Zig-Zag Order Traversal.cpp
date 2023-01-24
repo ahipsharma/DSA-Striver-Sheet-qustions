67c2c17bf35dd6735e60ab7197c7d539cb89f591
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-01-24 15:11:48 +0530

    DSA-Sheet Question.

---
+++ Trees/Medium Problems/BST Zig-Zag Order Traversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root == NULL)
+            return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag = true;
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> arr(n);
+            for(int i = 0;i<n;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                int idx = (flag) ? i : (n-i-1);
+                arr[idx] = node->val;
+                if(node->left)  q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            flag = !flag;
+            res.push_back(arr);
+        }
+        return res;
+    }
+};
