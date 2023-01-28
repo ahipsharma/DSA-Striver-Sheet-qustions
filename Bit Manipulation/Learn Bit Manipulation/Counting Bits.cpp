a3e7a27fcfa8b23b41abc1b9f62a9b9549c30091
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-01-28 12:34:50 +0530

    DSA-Sheet Question.

--- Bit Manipulation/Learn Bit Manipulation/Counting Bits.cpp
+++ Bit Manipulation/Learn Bit Manipulation/Counting Bits.cpp
@@ -1,15 +1,33 @@
-class Solution {
-public:
-    vector<int> countBits(int n) {
-        vector<int> res;
-        for(int i = 0; i<=n; i++){
-            int a = i, c = 0;
-            while(a){
-                a &= (a-1);
-                c++;
-            }
-            res.push_back(c);
+class Solution{
+    public:
+    int countSetBits(int n){
+        // int sum = 0, c;
+        // for(int i = 0; i<=n; i++){
+        //     int a = i, c = 0;
+        //     while(a){
+        //         a &= (a-1);
+        //         c++;
+        //     }
+        //     sum += c;
+        // }
+        // return sum;
+        
+        // Optimized:
+        if(n == 0)
+            return 0;
+        int power = largestPowerOf2InRange(n);
+        int bitstill2x = (1 << (power-1)) * power;
+        int msb = n - (1<<(power)) + 1;
+        int rest = n - (1<<power);
+        
+        int totBits = bitstill2x + msb + countSetBits(rest);
+        return totBits;
+    }
+    int largestPowerOf2InRange(int n){
+        int x = 0;
+        while((1<<x) <= n){
+            x++;
         }
-        return res;
+        return x-1;
     }
 };
