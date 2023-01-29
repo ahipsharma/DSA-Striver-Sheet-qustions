f868cb38ebcd3390fb64d06bcc278d42b589fe42
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-01-29 14:17:10 +0530

    DSA-Sheet Question.

---
+++ Recursion/Problems on Subsequences/Subset ||.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> printUniqueSubsets(vector<int>& nums) {
+        // Effective soln using Bit Manipulation.
+        int n = nums.size();
+        vector<vector<int>> res;
+
+        for(int i = 0; i<(1<<n); i++){
+            vector<int> ans;
+            for(int j = 0; j<n; j++){
+                if(i & (1<<j))
+                    ans.push_back(nums[j]);
+            }
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
