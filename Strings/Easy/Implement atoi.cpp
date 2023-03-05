0db3881a92538bf23b0a6caca3bb93c368651fe4
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-03-05 21:25:47 +0530

    DSA-Sheet QUestion.

---
+++ Strings/Easy/Implement atoi.cpp
@@ -0,0 +1,39 @@
+// Implement Atoi
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string s) {
+        int n = s.length();
+        int ans=0;
+        int i=0;
+        bool flag=false;
+        if(s[0]=='-'){
+            flag=true;
+            i++;
+        }
+        for(;i<n;i++){
+            if(s[i]<'0' or s[i]>'9') return -1;
+            int r = s[i]-'0';
+            ans = ans*10 + r;
+        }
+        if(flag) ans *= -1;
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.atoi(s)<<endl;
+    }
+    return 0;
+}
